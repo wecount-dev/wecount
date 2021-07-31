5d30a319169487ea0f5b5164f7e9c9078dfc8853
hyochan <dooboolab@gmail.com>
2021-07-31 13:21:37 +0900

    Organize the color assets

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,49 @@
 <style>
   :global(body) {
-    --button: #0084f6;
-    --text: black;
+    --green20: #CBFFDD;
+    --green30: #7EE2D3;
+    --green40: #28DB98;
+    --green70: #0DB293;
+    --green80: #00715B;
+    --green90: #074539;
+    --gray10: #F9F9F9;
+    --gray20: #F3F4F5;
+    --gray30: #E3E4E5;
+    --gray40: #D0D1D3;
+    --gray50: #B5B6B9;
+    --gray60: #8E9095;
+    --gray70: #76777D;
+    --gray80: #4C4D53;
+    --gray90: #2A2A2C;
+    --gray100: #1B1B1C;
+    --blue10: #F0F5FF;
+    --blue50: #3A74E7;
+    --blue90: #061178;
+    --yellow10: #FFFBE6;
+    --yellow50: #FFC53D;
+    --yellow90: #874D00;
+    --red10: #FFF2E8;
+    --red50: #F15454;
+    --red90: #871400;
 
-    background-color: var(--background-color);
-    color: var(--color);
+    --positive: var(--green50);
+    --negative: var(--red50);
+    --info: var(--blue50);
+    --warn: var(--yellow50);
+
+    --button: var(--green80);
+    --text: var(--gray100);
+
+    background-color: var(--gray10);
+    color: var(--gray100);
     transition: background-color 0.3s;
   }
   :global(body.dark-mode) {
-    --button: #069ccd;
-    --text: white;
+    --button: var(--green30);
+    --text: var(--gray10);
 
-    background-color: black;
-    color: white;
+    background-color: var(--gray100);
+    color: var(--gray10);
   }
 </style>
 
