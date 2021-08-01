b56db388a9915a4f5466d99169684ee37a758145
hyun <coderhyun476@gmail.com>
2021-08-01 18:14:31 +0900

    init

---
+++ src/components/uis/Modal.svelte
@@ -0,0 +1,29 @@
+<style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+</style>
+
+<script>
+  import {createEventDispatcher} from 'svelte';
+
+  export let visible;
+  export let style = '';
+
+  const dispatch = createEventDispatcher();
+
+  const click = () => {
+    dispatch('onBackdropClick');
+  };
+</script>
+
+{#if visible}
+  <div class="backdrop" style={style} on:click|self={click}>
+    <slot />
+  </div>
+{/if}
