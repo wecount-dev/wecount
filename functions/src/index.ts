d0452469e9581e0ff01030dc121a65e1755b0859
DevYuns <ydhun93@gmail.com>
2021-08-03 21:12:24 +0900

    Set up paypal payment environment & make a test createOrder feature

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,14 @@
-import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import {createOrder} from './paypal/index';
+
+admin.initializeApp();
+
+export const createPaypalOrder = functions.https.onRequest((req, res) => {
+  functions.logger.info('Hello logs!', {structuredData: true});
+
+  createOrder()
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
+});
