b8536839246aa810401b659c99efcd6c2c8f20dc
Jay-flow <kimpetro153@gmail.com>
2021-08-04 01:06:10 +0900

    Add colors properties for carousel

--- src/components/uis/Carousel/types.svelte
+++ src/components/uis/Carousel/types.svelte
@@ -1,19 +1,19 @@
 <script context="module" lang="ts">
-  export type CommunityType = {
-    name: string;
-    description: string;
-  };
-
   export type UserType = {
     name: string;
     role: string;
     imageUrl: string;
   };
 
+  export type CommunityType = {
+    name: string;
+    description: string;
+  };
+
   export type ItemType = {
     user: UserType;
     community: CommunityType;
     isPublic: boolean;
-    selectedColor: string;
+    colors: string[];
   };
 </script>
