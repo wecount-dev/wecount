02313098591f1fde9a8f199c6c24aefdb0d4ac9f
hyun <coderhyun476@gmail.com>
2021-08-06 00:09:35 +0900

    Import Member Screen TrashCan Icon

--- src/utils/Icon.ts
+++ src/utils/Icon.ts
@@ -3,9 +3,11 @@ import svgCheck from '../../assets/svgs/check.svg';
 import svgKey from '../../assets/svgs/key.svg';
 import svgLogo from '../../assets/svgs/logo.svg';
 import svgMail from '../../assets/svgs/mail.svg';
+import svgTrashCan from '../../assets/svgs/trash-can.svg';
 
 export const SvgCheck = svgCheck;
 export const SvgKey = svgKey;
 export const SvgLogo = svgLogo;
 export const SvgMail = svgMail;
 export const SvgArrowDown = svgArrowDown;
+export const SvgTrashcan = svgTrashCan;
