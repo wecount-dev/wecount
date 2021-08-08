ec1a051530408c6ed487b8e2f9e6e7f64816567e
Jay-flow <kimpetro153@gmail.com>
2021-08-09 01:28:41 +0900

    Bind data for drawer component
    Change data binding from slot to props

--- src/components/pages/Temp.svelte
+++ src/components/pages/Temp.svelte
@@ -1,10 +1,51 @@
-<style lang="postcss">
-  main {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-</style>
+<script lang="ts">
+  import Drawer from '../uis/Drawer/Drawer.svelte';
 
-<main>Temp</main>
+  let string = `This is <strong>Temp!</strong>`;
+
+  let item = {
+    community: {
+      imageUrl:
+        'https://cdn.icon-icons.com/icons2/2620/PNG/512/among_us_player_red_icon_156942.png',
+    },
+    menu: [
+      {
+        name: '대시보드',
+        url: 'https://localhost:5000/dashboard',
+      },
+      {
+        name: '피드',
+        notificationCounts: 99,
+        url: 'https://localhost:5000/feed',
+      },
+    ],
+  };
+
+  let item2 = {
+    community: {
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png',
+    },
+    menu: [
+      {
+        name: '커밋',
+        url: 'https://localhost:5000/commit',
+      },
+      {
+        name: '이슈',
+        notificationCounts: 99,
+        url: 'https://localhost:5000/issue',
+      },
+    ],
+  };
+
+  let items = [item, item2];
+</script>
+
+<div style="padding: 30px; background-color:#F2F5F6;">
+  <p>{@html string}</p>
+
+  <div>
+    <Drawer items={items} />
+  </div>
+</div>
