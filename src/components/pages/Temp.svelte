d528a18349a8996990ad2d5430ed495c41af26ff
hyochan <dooboolab@gmail.com>
2021-08-08 21:14:45 +0900

    Remove code in [Temp]

--- src/components/pages/Temp.svelte
+++ src/components/pages/Temp.svelte
@@ -7,47 +7,4 @@
   }
 </style>
 
-<script lang="ts">
-  import Button from '../uis/Button.svelte';
-  import EditText from '../uis/EditText.svelte';
-  import {createCommunity} from '../../services/communityService';
-  import {user} from '../../stores/sessionStore';
-
-  const requestCreateCommunity = async () => {
-    if ($user) {
-      const community = await createCommunity({
-        name: 'dooboolab-community',
-        currency: 'KRW',
-        description: 'Hi dooboolab',
-        isPublic: true,
-        userId: $user.id,
-      });
-
-      console.log('community', community);
-    }
-  };
-</script>
-
-<main>
-  <EditText
-    containerStyle="width: 80%;"
-    inputStyle="font-size: 14px;"
-    type="text"
-  />
-  <Button
-    on:click={requestCreateCommunity}
-    style="color: black; font-size: 14px; align-self: stretch; margin: 12px 10%;"
-  >
-    <div>Create Community</div>
-  </Button>
-  <Button
-    style="color: black; font-size: 14px; align-self: stretch; margin: 12px 10%;"
-  >
-    <div>Update Community</div>
-  </Button>
-  <Button
-    style="color: black; font-size: 14px; align-self: stretch; margin: 12px 10%;"
-  >
-    <div>Delete Community</div>
-  </Button>
-</main>
+<main>Temp</main>
