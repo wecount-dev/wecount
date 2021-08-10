065450a8d588f2983f42c3d7711078d35151807e
Jay-flow <kimpetro153@gmail.com>
2021-08-10 22:18:19 +0900

    Revert "Merge branch 'master' into feat/carousel"
    This reverts commit 3e751664f97666e139768d279c9e1ad8eab36ead, reversing
    changes made to e5e34103fad954617660771d6c21fa6dffd36ac0.

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,8 +1,11 @@
-module.exports = {
-  root: true,
-  extends: '@dooboo/eslint-config',
-  rules: {
-    'eslint-comments/no-unlimited-disable': 0,
-    'eslint-comments/no-unused-disable': 0,
-  },
-};
+{
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+}
