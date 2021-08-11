8314dc7b4e8830b4dd795f92e004ee7b0c8a472d
Jay-flow <kimpetro153@gmail.com>
2021-08-11 23:14:35 +0900

    Rename icon svg files from Kebab case to snake case

--- src/utils/Icon.ts
+++ src/utils/Icon.ts
@@ -1,14 +1,14 @@
-import svgArrowDown from '../../assets/svgs/arrow-down.svg';
-import svgBadgeCrown from '../../assets/svgs/badge-crown.svg';
+import svgArrowDown from '../../assets/svgs/arrow_down.svg';
+import svgBadgeCrown from '../../assets/svgs/badge_crown.svg';
 import svgCheck from '../../assets/svgs/check.svg';
 import svgCrown from '../../assets/svgs/crown.svg';
 import svgKey from '../../assets/svgs/key.svg';
-import svgLeftAngleBracket from '../../assets/svgs/left-angle-bracket.svg';
+import svgLeftAngleBracket from '../../assets/svgs/left_angle_bracket.svg';
 import svgLock from '../../assets/svgs/lock.svg';
 import svgLogo from '../../assets/svgs/logo.svg';
 import svgMail from '../../assets/svgs/mail.svg';
-import svgRightAngleBracket from '../../assets/svgs/right-angle-bracket.svg';
-import svgTrashCan from '../../assets/svgs/trash-can.svg';
+import svgRightAngleBracket from '../../assets/svgs/right_angle_bracket.svg';
+import svgTrashCan from '../../assets/svgs/trash_can.svg';
 
 export const SvgCheck = svgCheck;
 export const SvgKey = svgKey;
