86cae53aa77da5036cdf31303f21c5c592d640a4
Jay-flow <kimpetro153@gmail.com>
2021-08-12 22:23:34 +0900

    Add animation when clicking the add community button in the drawer component

--- src/components/uis/Drawer/CommunityPlusMenu.svelte
+++ src/components/uis/Drawer/CommunityPlusMenu.svelte
@@ -1,3 +1,20 @@
+<style lang="postcss">
+  :global(.community-plus-menu) {
+    background-color: var(--gray20);
+    background-position: center;
+    transition: background 0.8s;
+  }
+  :global(.community-plus-menu):hover {
+    background: var(--gray30)
+      radial-gradient(circle, transparent 1%, var(--gray30) 1%) center/15000%;
+  }
+  :global(.community-plus-menu):active {
+    background-color: var(--gray20);
+    background-size: 100%;
+    transition: background 0s;
+  }
+</style>
+
 <script lang="ts">
   import SelectIconButton from './SelectIconButton.svelte';
   import {SvgPlus} from '../../../utils/Icon';
