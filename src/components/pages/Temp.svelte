e8c09c58a85caa1a7e60ce00314750d3b54c136a
Jay-flow <kimpetro153@gmail.com>
2021-08-18 00:42:34 +0900

    Change the menu data from the drawer component to constant

--- src/components/pages/Temp.svelte
+++ src/components/pages/Temp.svelte
@@ -1,31 +1,10 @@
-<script lang="ts">
-  import Drawer from '../uis/Drawer/Drawer.svelte';
+<style lang="postcss">
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
 
-  const string = `This is <strong>Temp!</strong>`;
-
-  const communites = [
-    {
-      id: '1',
-      imageUrl:
-        'https://cdn.icon-icons.com/icons2/2620/PNG/512/among_us_player_red_icon_156942.png',
-    },
-    {
-      id: '2',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png',
-    },
-  ];
-
-  const onSelectMenu = (url: string) => {
-    // eslint-disable-next-line no-console
-    console.log(`Selected URL: ${url}`);
-  };
-</script>
-
-<div style="padding: 30px; background-color:#F2F5F6;">
-  <p>{@html string}</p>
-
-  <div>
-    <Drawer communites={communites} onSelectMenu={onSelectMenu} />
-  </div>
-</div>
+<main>Temp</main>
