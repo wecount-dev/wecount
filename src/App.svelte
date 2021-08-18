438abbe36c288afcb860c4e126b6dd3a676a4917
Jay-flow <kimpetro153@gmail.com>
2021-08-19 00:00:48 +0900

    Add drawer component dark mode

--- src/App.svelte
+++ src/App.svelte
@@ -47,6 +47,7 @@
     transition: background-color 0.3s;
   }
   :global(body.dark-mode) {
+    --primary: var(--green70);
     --button: var(--green30);
     --placeholder: var(--gray80);
     --text: var(--gray10);
