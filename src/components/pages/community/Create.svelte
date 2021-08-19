3f638abfdfa873dee734d47334664b7bbccf3163
Jay-flow <kimpetro153@gmail.com>
2021-08-20 00:46:22 +0900

    Write down the title of the community creation page

--- src/components/pages/community/Create.svelte
+++ src/components/pages/community/Create.svelte
@@ -1,6 +1,25 @@
-<style lang="postcss"></style>
+<style lang="postcss">
+  .container {
+    display: flex;
+    justify-content: center;
+  }
+  .wrap {
+    width: 620px;
+  }
+  .title {
+    margin-top: 40px;
+    margin-bottom: 22px;
+  }
+</style>
 
 <script lang="ts">
+  import {_} from 'svelte-i18n';
 </script>
 
-<div>Hello world</div>
+<div class="container">
+  <div class="wrap">
+    <h3 class="title">
+      {$_('app_name')}<br />{$_('Community.create_community')}
+    </h3>
+  </div>
+</div>
