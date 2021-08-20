f8420570d448a77de9066e104e723c28141343a6
Jay-flow <kimpetro153@gmail.com>
2021-08-21 00:11:16 +0900

    Create CircleImage component

--- src/components/pages/dashboard/Index.svelte
+++ src/components/pages/dashboard/Index.svelte
@@ -1,7 +1,20 @@
 <style lang="postcss">
+  .temp {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
 
 <script lang="ts">
+  import Card from '../../uis/Card.svelte';
+  import CircleImage from '../../uis/CircleImage.svelte';
 </script>
 
-<div>Dashboard</div>
+<div class="temp">
+  <div>
+    <Card>
+      <CircleImage src={'https://www.w3schools.com/howto/img_avatar.png'} />
+    </Card>
+  </div>
+</div>
