5d550c6cb208946546b39d6064e901740e0b7738
Jay-flow <kimpetro153@gmail.com>
2021-08-21 00:56:27 +0900

    Adjust Image Ratio to CircleImage Component

--- src/components/pages/dashboard/MemberCard.svelte
+++ src/components/pages/dashboard/MemberCard.svelte
@@ -7,29 +7,38 @@
     display: flex;
     align-items: center;
     justify-content: space-between;
+    margin-bottom: 12px;
   }
-  .member-management {
-    font-size: 0.75rem;
-    color: var(--link);
+  .members {
+    display: flex;
+    justify-content: space-between;
   }
 </style>
 
 <script lang="ts">
   import {_} from 'svelte-i18n';
-  import {SvgSmallRightAngleBracket} from '../../../utils/Icon';
   import Card from '../../uis/Card.svelte';
   import CircleImage from '../../uis/CircleImage.svelte';
+  import TextLink from './TextLink.svelte';
+
+  const members = [
+    'https://www.w3schools.com/howto/img_avatar.png',
+    'https://www.w3schools.com/howto/img_avatar.png',
+    'https://www.w3schools.com/howto/img_avatar.png',
+    'https://www.w3schools.com/howto/img_avatar.png',
+  ];
 </script>
 
 <div class="member-card">
   <Card cardStyle={'padding: 28px;'}>
     <div class="header">
       <span class="sub-heading">{$_('member')}</span>
-      <div class="member-management">
-        {$_('member')}
-        <SvgSmallRightAngleBracket />
-      </div>
+      <TextLink name={$_('Dashboard.manage_members')} href={'/member'} />
     </div>
-    <CircleImage src={'https://www.w3schools.com/howto/img_avatar.png'} />
+    <div class="members">
+      {#each members as member}
+        <CircleImage src={member} style={'width: 48px;'} />
+      {/each}
+    </div>
   </Card>
 </div>
