b03ea1137b05cb949d54d29835c32722747ff5f0
Jay-flow <kimpetro153@gmail.com>
2021-08-25 22:02:09 +0900

    Remove unused CSS

--- src/App.svelte
+++ src/App.svelte
@@ -49,10 +49,6 @@
     background-color: var(--background-color);
     color: var(--gray100);
     transition: background-color 0.3s;
-
-    :global(.community-route) {
-      background-color: #f2f5f6;
-    }
   }
   :global(body.dark-mode) {
     --button: var(--green30);
@@ -66,10 +62,6 @@
 
     background-color: var(--background-color);
     color: var(--gray10);
-
-    :global(.community-route) {
-      background-color: var(--gray90);
-    }
   }
   * {
     font-family: 'Spoqa Han Sans Neo';
