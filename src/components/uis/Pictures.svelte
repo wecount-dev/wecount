ee185db56f85187d8c08e196f050f1014ce9a0cf
hyun <coderhyun476@gmail.com>
2021-08-29 19:21:26 +0900

    Code Refectoring Pictures.svelte

--- src/components/uis/Pictures.svelte
+++ src/components/uis/Pictures.svelte
@@ -2,124 +2,76 @@
   main {
     cursor: pointer;
   }
-  .len1 {
+  .image {
+    background-size: cover;
+    grid-area: image1;
+  }
+  .image5-wrapper {
+    grid-area: image5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(0, 0, 0, 0.7);
+    color: var(--gray10);
+    font-size: 28px;
+    font-weight: bold;
+  }
+
+  .container {
     width: 100%;
     position: relative;
-    .image {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      cursor: pointer;
-      background-size: cover;
-    }
   }
-  .len1:after {
+  .container:after {
     content: '';
     display: block;
     padding-bottom: 100%;
   }
-
-  .len2 {
-    width: 100%;
-    position: relative;
-    .container {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      display: grid;
-      grid-template-columns: 1fr 10px 1fr;
-      grid-template-rows: 1fr;
-      grid-template-areas: 'image1 . image2';
-    }
-    .image1 {
-      grid-area: image1;
-    }
-    .image2 {
-      grid-area: image2;
-    }
-  }
-  .len2:after {
-    content: '';
-    display: block;
+  .container-len2:after {
     padding-bottom: calc(50% - 10px);
   }
-
-  .len3 {
-    width: 100%;
-    position: relative;
-    .container {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      display: grid;
-      grid-template-columns: 1fr 10px 1fr;
-      grid-template-rows: 1fr 10px 1fr;
-      grid-template-areas:
-        'image1 . image2'
-        '. . .'
-        'image3 image3 image3';
-      .image1 {
-        grid-area: image1;
-      }
-      .image2 {
-        grid-area: image2;
-      }
-      .image3 {
-        grid-area: image3;
-      }
-    }
-  }
-  .len3:after {
-    content: '';
-    display: block;
-    padding-bottom: 100%;
-  }
-
-  .len5 {
-    width: 100%;
-    position: relative;
-    .container {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      display: grid;
-      grid-template-columns: 1fr 10px 1fr 10px 1fr 10px 1fr 10px 1fr 10px 1fr;
-      grid-template-rows: 3fr 10px 2fr;
-      grid-template-areas:
-        'image1 image1 image1 image1 image1 . image2 image2 image2 image2 image2'
-        '. . . . . . . . . . .'
-        'image3 image3 image3 . image4 image4 image4 . image5 image5 image5';
-      .image1 {
-        grid-area: image1;
-      }
-      .image2 {
-        grid-area: image2;
-      }
-      .image3 {
-        grid-area: image3;
-      }
-      .image4 {
-        grid-area: image4;
-      }
-      .image5 {
-        grid-area: image5;
-      }
-      .image5-wrapper {
-        grid-area: image5;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: rgba(0, 0, 0, 0.7);
-        color: var(--gray10);
-        font-size: 28px;
-        font-weight: bold;
-      }
-    }
-  }
-  .len5:after {
-    content: '';
-    display: block;
+  .container-len5:after {
     padding-bottom: calc((5 / 6) * 100%);
+  }
+
+  .inner-container {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: grid;
+  }
+  .inner-len1 {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+    grid-template-areas: 'image1';
+  }
+  .inner-len2 {
+    grid-template-columns: 1fr 10px 1fr;
+    grid-template-rows: 1fr;
+    grid-template-areas: 'image1 . image2';
+  }
+  .inner-len3 {
+    grid-template-columns: 1fr 10px 1fr;
+    grid-template-rows: 1fr 10px 1fr;
+    grid-template-areas:
+      'image1 . image2'
+      '. . .'
+      'image3 image3 image3';
+  }
+  .inner-len4 {
+    grid-template-columns: 1fr 10px 1fr;
+    grid-template-rows: 1fr 10px 1fr;
+    grid-template-areas:
+      'image1 . image2'
+      '. . .'
+      'image3 . image4';
+  }
+  .inner-len5 {
+    grid-template-columns: 1fr 10px 1fr 10px 1fr 10px 1fr 10px 1fr 10px 1fr;
+    grid-template-rows: 3fr 10px 2fr;
+    grid-template-areas:
+      'image1 image1 image1 image1 image1 . image2 image2 image2 image2 image2'
+      '. . . . . . . . . . .'
+      'image3 image3 image3 . image4 image4 image4 . image5 image5 image5';
   }
 </style>
 
@@ -127,6 +79,8 @@
   import PicturesModal from './PicturesModal.svelte';
 
   export let urls: string[];
+
+  $: slicedUrls = urls.slice(0, 5);
 
   let pictureModalVisible = false;
   let clickedIndex = 0;
@@ -137,108 +91,33 @@
   };
 </script>
 
-<main>
-  <PicturesModal
-    bind:visible={pictureModalVisible}
-    currentIndex={clickedIndex}
-    urls={urls}
-  />
-  {#if urls.length === 1}
-    <div class="len1">
+{#if urls.length > 0}
+  <main>
+    <PicturesModal
+      bind:visible={pictureModalVisible}
+      currentIndex={clickedIndex}
+      urls={urls}
+    />
+    <div
+      class:container-len2={slicedUrls.length === 2}
+      class:container-len5={slicedUrls.length === 5}
+      class="container"
+    >
       <div
-        class="image"
-        on:click={() => onClick(0)}
-        style={`background-image: url("${urls[0]}");`}
-      />
-    </div>
-  {:else if urls.length === 2}
-    <div class="len2">
-      <div class="container">
-        <div
-          class="image1"
-          on:click={() => onClick(0)}
-          style={`background-image: url("${urls[0]}");`}
-        />
-        <div
-          class="image2"
-          on:click={() => onClick(1)}
-          style={`background-image: url("${urls[1]}");`}
-        />
-      </div>
-    </div>
-  {:else if urls.length === 3}
-    <div class="len3">
-      <div class="container">
-        <div
-          class="image1"
-          on:click={() => onClick(0)}
-          style={`background-image: url("${urls[0]}");`}
-        />
-        <div
-          class="image2"
-          on:click={() => onClick(1)}
-          style={`background-image: url("${urls[1]}");`}
-        />
-        <div
-          class="image3"
-          on:click={() => onClick(2)}
-          style={`background-image: url("${urls[2]}");`}
-        />
-      </div>
-    </div>
-  {:else if urls.length === 4}
-    <div class="len4">
-      <div class="container">
-        <div
-          class="image1"
-          on:click={() => onClick(0)}
-          style={`background-image: url("${urls[0]}");`}
-        />
-        <div
-          class="image2"
-          on:click={() => onClick(1)}
-          style={`background-image: url("${urls[1]}");`}
-        />
-        <div
-          class="image3"
-          on:click={() => onClick(2)}
-          style={`background-image: url("${urls[2]}");`}
-        />
-        <div
-          class="image4"
-          on:click={() => onClick(3)}
-          style={`background-image: url("${urls[3]}");`}
-        />
-      </div>
-    </div>
-  {:else if urls.length >= 5}
-    <div class="len5">
-      <div class="container">
-        <div
-          class="image1"
-          on:click={() => onClick(0)}
-          style={`background-image: url("${urls[0]}");`}
-        />
-        <div
-          class="image2"
-          on:click={() => onClick(1)}
-          style={`background-image: url("${urls[1]}");`}
-        />
-        <div
-          class="image3"
-          on:click={() => onClick(2)}
-          style={`background-image: url("${urls[2]}");`}
-        />
-        <div
-          class="image4"
-          on:click={() => onClick(3)}
-          style={`background-image: url("${urls[3]}");`}
-        />
-        <div
-          class="image5"
-          on:click={() => onClick(4)}
-          style={`background-image: url("${urls[4]}");`}
-        />
+        class:inner-len1={slicedUrls.length === 1}
+        class:inner-len2={slicedUrls.length === 2}
+        class:inner-len3={slicedUrls.length === 3}
+        class:inner-len4={slicedUrls.length === 4}
+        class:inner-len5={slicedUrls.length === 5}
+        class={`inner-container`}
+      >
+        {#each slicedUrls as url, i}
+          <div
+            class="image"
+            on:click={() => onClick(i)}
+            style={`grid-area: image${i + 1}; background-image: url("${url}");`}
+          />
+        {/each}
         {#if urls.length > 5}
           <div class="image5-wrapper" on:click={() => onClick(4)}>
             +{urls.length - 5}
@@ -246,5 +125,5 @@
         {/if}
       </div>
     </div>
-  {/if}
-</main>
+  </main>
+{/if}
